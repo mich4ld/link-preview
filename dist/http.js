2078702bd9298fcb227af547fa58cc63e23c791c
mich4ld <mchldziuba@gmail.com>
2021-09-13 11:09:22 +0200

    init files

---
+++ dist/http.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HttpClient = void 0;
+var HttpClient = /** @class */ (function () {
+    function HttpClient() {
+    }
+    return HttpClient;
+}());
+exports.HttpClient = HttpClient;
