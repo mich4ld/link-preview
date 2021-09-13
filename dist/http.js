927c92e09ab6b118b3c90721483a633ea69dd2f1
Michał <43048524+mich4ld@users.noreply.github.com>
2021-09-13 11:13:51 +0200

    Delete dist directory

--- dist/http.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HttpClient = void 0;
-var HttpClient = /** @class */ (function () {
-    function HttpClient() {
-    }
-    return HttpClient;
-}());
-exports.HttpClient = HttpClient;
